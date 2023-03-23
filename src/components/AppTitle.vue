ccba610cdf695fe2a0b438f9239f71b7830ce40a
Fez06 <mattiafezzardi1993@gmail.com>
2023-03-23 01:57:48 +0100

    commit

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,20 @@
+<script>
+    export default{
+        data() {
+            return {
+                nuovo: 'La mia prima app con Vite!'
+            }
+        }
+    }
+</script>
+
+<template>
+    <h1>{{nuovo}}</h1>
+</template>
+
+<style scoped>
+    h1 {
+        text-align: center;
+        color: rgb(194, 78, 113);
+    }
+</style>
